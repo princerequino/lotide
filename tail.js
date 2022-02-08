5900cd630cfdfe3524ff8acae3b5eca9eddc4763
princerequino <requino.prince@gmail.com>
2022-02-07 19:05:50 -0500

    complete and tested tail.js

---
+++ tail.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(input){
+  let newAr = input.slice(1);
+  return newAr
+};
+
+// Test Case: Check the original array 
+const words = ["Yo Yo"];
+const emptyArray = [];
+tail(words); 
+assertEqual(words.length, 3); 
+assertEqual(["Yo Yo"], tail(["Yo Yo"]));
+assertEqual(["Yo Yo", "Lighthouse", "Labs"], tail(["Yo Yo", "Lighthouse", "Labs"]));
+
