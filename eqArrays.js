3739a3de0c6a55e5a4930d424f83b279d830da6b
princerequino <requino.prince@gmail.com>
2022-02-14 14:11:44 -0500

    module.exports implemented

--- eqArrays.js
+++ eqArrays.js
@@ -6,6 +6,19 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+module.exports = eqArrays;
+
+
 //A much-much-much unnecessary longer method
 
 /*const eqArrays = function (array1, array2){
@@ -24,28 +37,3 @@ const assertEqual = function(actual, expected) {
     }
     return true;
     };*/
-
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-
-
-
-// TEST CODE
-//assertEqual("Lighthouse Labs", "Bootcamp");
-//assertEqual(1, 1);
-
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
