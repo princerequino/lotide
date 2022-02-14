76031cb7d093c748fad7c950e8d6c69bd92622d6
princerequino <requino.prince@gmail.com>
2022-02-14 13:55:08 -0500

    test code removed and module.export implemented

--- head.js
+++ head.js
@@ -1,18 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `✅✅✅Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    return `🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
-
 const head = function(array) {
   let x = array[0];
-  return x
+  return x;
 };
 
-// TEST CODE
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head([])));
-console.log(assertEqual(head([2]),2));
+module.exports = head;
+
