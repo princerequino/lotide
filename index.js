b31981c1fea7e936b4623906907d64bce1f41be6
princerequino <requino.prince@gmail.com>
2022-02-14 16:16:56 -0500

    reduced index.js using shorthand ES6

---
+++ index.js
@@ -0,0 +1,39 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectEqual = require('./assertObjectEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const piglatin = require('./piglatin');
+const reverse = require('./reverse');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  piglatin,
+  reverse,
+  takeUntil,
+  without
+};
